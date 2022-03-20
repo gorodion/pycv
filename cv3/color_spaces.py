8b7552ebf32aa93a401d98fce4ffe2d45edcb932
gorodion <64599413+gorodion@users.noreply.github.com>
2022-03-20 23:50:28 +0300

    1.1 typing + round->int

--- cv3/color_spaces.py
+++ cv3/color_spaces.py
@@ -1,6 +1,7 @@
 from functools import partial
 import cv2
 import numpy as np
+from ._utils import type_decorator
 
 __all__ = [
     'rgb',
@@ -17,18 +18,21 @@ __all__ = [
     'hsv2bgr',
     'hsv2rgb',
 ]
+
+@type_decorator
 def rgb(img: np.ndarray):
     if img.ndim != 3:  # only if 3-color image
         return img
     return cv2.cvtColor(img, code=cv2.COLOR_BGR2RGB)
 
 
+_cvtColor = type_decorator(cv2.cvtColor)
 rgb2bgr = bgr2rgb = bgr = rgb
-bgr2gray = partial(cv2.cvtColor, code=cv2.COLOR_BGR2GRAY)
-rgb2gray = partial(cv2.cvtColor, code=cv2.COLOR_RGB2GRAY)
-gray2rgb = partial(cv2.cvtColor, code=cv2.COLOR_GRAY2RGB)
-gray2bgr = partial(cv2.cvtColor, code=cv2.COLOR_GRAY2BGR)
-bgr2hsv = partial(cv2.cvtColor, code=cv2.COLOR_BGR2HSV)
-rgb2hsv = partial(cv2.cvtColor, code=cv2.COLOR_RGB2HSV)
-hsv2bgr = partial(cv2.cvtColor, code=cv2.COLOR_HSV2BGR)
-hsv2rgb = partial(cv2.cvtColor, code=cv2.COLOR_HSV2RGB)
+bgr2gray = partial(_cvtColor, code=cv2.COLOR_BGR2GRAY)
+rgb2gray = partial(_cvtColor, code=cv2.COLOR_RGB2GRAY)
+gray2rgb = partial(_cvtColor, code=cv2.COLOR_GRAY2RGB)
+gray2bgr = partial(_cvtColor, code=cv2.COLOR_GRAY2BGR)
+bgr2hsv = partial(_cvtColor, code=cv2.COLOR_BGR2HSV)
+rgb2hsv = partial(_cvtColor, code=cv2.COLOR_RGB2HSV)
+hsv2bgr = partial(_cvtColor, code=cv2.COLOR_HSV2BGR)
+hsv2rgb = partial(_cvtColor, code=cv2.COLOR_HSV2RGB)
